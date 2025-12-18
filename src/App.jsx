cf0e11acf8223526e47868e871db5be66bf483d8
Erin Scott <212621843+scotterina@users.noreply.github.com>
2025-12-17 21:43:25 -0500

    week 4 lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import './TodoList';
+import { useState } from 'react';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 import './App.css';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('My first todo');
+
   return (
     <div>
       <h1>My Todos</h1>
+      <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
-      <ul>
-        todos.map((todo) = (<li key={todo.id}>{todo.title}</li>
-        ))
-        <TodoList />
-      </ul>
     </div>
   );
 }
