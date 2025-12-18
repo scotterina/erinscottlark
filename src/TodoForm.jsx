e9887d685e6ba4262afb9a0648a21cfec0869bfc
Erin Scott <212621843+scotterina@users.noreply.github.com>
2025-12-17 19:56:39 -0500

    week 2 edit 3

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,17 +1,13 @@
-function TodoForm() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <TodoList />
-    </div>
-  );
-  return (
-    <form>
-      <label htmlFor="todoTitle">Todo</label>
-      <input id="todoTitle" />
-      <button>Add Todo</button>
-    </form>
-  );
-}
+<div>
+  <h1>My Todos</h1>
+  <TodoList />
+</div>;
+return (
+  <form>
+    <label htmlFor="todoTitle">Todo</label>
+    <input id="todoTitle" />
+    <button>Add Todo</button>
+  </form>
+);
 
 export default TodoForm;
