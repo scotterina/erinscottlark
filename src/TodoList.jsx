a8f70ffbced141768ba4c694c7fc436ab604a82d
Erin Scott <212621843+scotterina@users.noreply.github.com>
2025-12-19 12:07:27 -0500

    updated version

---
+++ src/TodoList.jsx
@@ -0,0 +1,18 @@
+import TodoListItem from './TodoListItem';
+
+function TodoList() {
+  const todos = [
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ];
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
