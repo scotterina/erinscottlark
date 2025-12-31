abda4ae68aebca15c9891a8ac970dc38571d1000
Erin Scott <212621843+scotterina@users.noreply.github.com>
2025-12-30 20:01:00 -0500

    updated jsx files with components

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,13 +1,11 @@
-<div>
-  <h1>My Todos</h1>
-  <TodoList />
-</div>;
-return (
-  <form>
-    <label htmlFor="todoTitle">Todo</label>
-    <input id="todoTitle" />
-    <button>Add Todo</button>
-  </form>
-);
+function TodoForm() {
+  return (
+    <form>
+      <label htmlFor="todoTitle">Todo</label>
+      <input id="todoTitle" />
+      <button>Add Todo</button>
+    </form>
+  );
+}
 
 export default TodoForm;
